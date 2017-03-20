bae5bb0466484922d6130c860d064d52325fb5d3
Roman.luzko <luzko.roman@it-wk.ru>
2017-03-20 13:57:33 +0300

    test

--- src/main/java/com/wk/config/RabbitConfiguration.java
+++ src/main/java/com/wk/config/RabbitConfiguration.java
@@ -1,6 +1,5 @@
 package com.wk.config;
 
-
 import org.springframework.amqp.core.AmqpAdmin;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
@@ -12,7 +11,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-
 
 /**
  * @author Roman Luzko
